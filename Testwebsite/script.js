92d7d0104ad17f54d0e28d54bf590cd1b766e354
EveTheOne <zacalkaaby@hotmail.com>
2021-11-08 14:39:22 +1100

    main

--- Testwebsite/script.js
+++ Testwebsite/script.js
@@ -1,34 +1,15 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 2f3331d393114657ead9c26bd3d1ba51b20cdc63
 function onLoad() {
     const btn = document.querySelector('.nav-button')
-    const nav = document.querySelector('.navigation ')
-
-    btn.addEventListener("click", function() {
-        if(nav.classList.contains('open') ) {
-            nav.classList.remove('open')
-        }
-        else {
-            nav.classList.add('open')
-        }
-    }   )   
-}
-<<<<<<< HEAD
-=======
-function onLoad() {
-    const btn = document.querySelector('.nav-button')
-    const nav = document.querySelector('.navigation ')
-
-    btn.addEventListener("click", function() {
-        if(nav.classList.contains('open') ) {
-            nav.classList.remove('open')
-        }
-        else {
-            nav.classList.add('open')
-        }
-    }   )   
-
->>>>>>> 2f3331d393114657ead9c26bd3d1ba51b20cdc63
-window.addEventListener('load', onLoad)
+    const nav = document.querySelector('.navigation')
+  
+    btn.addEventListener('click', function () {
+      if (nav.classList.contains('open')) {
+        nav.classList.remove('open')
+      }
+      else {
+        nav.classList.add('open')
+      }
+    }
+    )
+  }
+  window.addEventListener('load', onLoad)
